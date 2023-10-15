3d56177b5274e8e004269fe0b183e815ff462191
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2023-10-15 18:02:36 -0300

    Estilizando

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,16 @@ function Header(){
   return(
     <header>
       <div>
-        <img src="https://github.com/gabrieszin.png" alt="Foto de Mayk Brito na frente de um laptop" />
+        <a 
+          href="https://github.com/gabrieszin" 
+          rel="noreferrer noopener" 
+          data-toggle="tooltip" 
+          data-placement="top" 
+          data-bs-custom-class="custom-tooltip" 
+          title="Eu no GitHub"
+        >
+          <img src="https://github.com/gabrieszin.png" alt="Foto de Mayk Brito na frente de um laptop" />
+        </a>
       </div>
     </header>
   )
